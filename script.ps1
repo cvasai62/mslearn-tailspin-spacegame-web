ad9cc6e03503a782c8acaf8a2a3628bf09e4a1f0
ajayvamsisree <ajayvamsisree@gmail.com>
2020-07-03 10:39:08 +0530

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,9 @@
+git clone "https://github.com/ajayvamsisree/test.git"
+cd test
+git status
+echo "hello 123" >>abc.txt
+git status
+git add abc.txt
+git commit -m "add"
+git push -u origin master
+git status
